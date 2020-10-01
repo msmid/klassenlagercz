fed13b97c83b1ccd8f9dc4366ea0305d6f4444cf
Tomáš Witek <tomas@witek.cz>
2020-10-01 17:24:56 +0200

    Fetch activities via static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../src/components/Footer";
 import Header, { Klassenlager } from "../src/components/Header";
 import Team, { TeamMember } from "../src/components/Team";
-import Timeline from "../src/components/Timeline";
+import Timeline, { Activity } from "../src/components/Timeline";
 import { apolloClient } from "../src/config";
 import {
   GET_ACTIVE_KLASSENLAGER,
@@ -14,16 +14,17 @@ import dayjs from "dayjs";
 interface HomeProps {
   klassenlagers: Klassenlager[];
   teamMembers: TeamMember[];
+  activities: Activity[];
 }
 
-const Home = ({ klassenlagers, teamMembers }: HomeProps) => {
+const Home = ({ klassenlagers, teamMembers, activities }: HomeProps) => {
   return (
     <>
       <Header klassenlager={klassenlagers[0]} />
 
       <Team teamMembers={teamMembers} />
 
-      <Timeline />
+      <Timeline activities={activities} />
 
       <Footer />
     </>
@@ -38,7 +39,10 @@ export async function getStaticProps(context) {
   const { data: { teamMembers } = {} } = await apolloClient.query({
     query: GET_TEAM_MEMBERS_WITH_PICTURES,
   });
-  return { props: { klassenlagers, teamMembers } };
+  const { data: { activities } = {} } = await apolloClient.query({
+    query: GET_ACTIVITIES,
+  });
+  return { props: { klassenlagers, teamMembers, activities } };
 }
 
 export default Home;
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
 import { Container, Row, Col, Spinner } from "react-bootstrap";
-import { GET_ACTIVITIES } from "../graphql/queries";
 import TimelineItem from "./TimelineItem";
 import _ from "lodash";
 import dayjs from "dayjs";
 
-interface Props {}
+export interface Activity {
+  id: string;
+  datetime: string;
+  description: string;
+  program: any;
+  picture: any;
+}
 
-const Timeline: React.FC<Props> = () => {
-  const { loading, error, data } = useQuery(GET_ACTIVITIES);
+interface TimelineProps {
+  activities: Activity[];
+}
 
-  let timeline = data?.activities;
+function Timeline({ activities }) {
+  let timeline = activities;
   let timelineKeys = null;
 
   if (timeline) {
     timeline = timeline
       .slice()
-      .sort((a, b) => Number(new Date(a.program.day)) - Number(new Date(b.program.day)));
+      .sort(
+        (a, b) =>
+          Number(new Date(a.program.day)) - Number(new Date(b.program.day))
+      );
     timeline = _.groupBy(timeline, "program.day");
     timelineKeys = Object.keys(timeline);
   }
@@ -28,44 +37,36 @@ const Timeline: React.FC<Props> = () => {
 
   return (
     <Container>
-      {loading ? (
-        <Spinner animation="border" />
-      ) : (
-        <>
-          <Row>
-            <Col>
-              <h2>Timeline</h2>
+      <Row>
+        <Col>
+          <h2>Timeline</h2>
+        </Col>
+      </Row>
+      {timelineKeys?.map((day, key) => {
+        return (
+          <Row key={key} className="pb-5">
+            <Col sm={12}>
+              <h3 className="text-primary mb-3">{dayjs(day).format("dddd")}</h3>
+            </Col>
+            <Col sm={12}>
+              <Row>
+                {timeline[day].sort(sortByDatetime).map((item, key) => {
+                  return (
+                    <Col key={key} sm={12}>
+                      <TimelineItem
+                        item={item}
+                        last={key === timeline[day].length - 1}
+                      />
+                    </Col>
+                  );
+                })}
+              </Row>
             </Col>
           </Row>
-          {timelineKeys?.map((day, key) => {
-            return (
-              <Row key={key} className="pb-5">
-                <Col sm={12}>
-                  <h3 className="text-primary mb-3">
-                    {dayjs(day).format("dddd")}
-                  </h3>
-                </Col>
-                <Col sm={12}>
-                  <Row>
-                    {timeline[day].sort(sortByDatetime).map((item, key) => {
-                      return (
-                        <Col key={key} sm={12}>
-                          <TimelineItem
-                            item={item}
-                            last={key === timeline[day].length - 1}
-                          />
-                        </Col>
-                      );
-                    })}
-                  </Row>
-                </Col>
-              </Row>
-            );
-          })}
-        </>
-      )}
+        );
+      })}
     </Container>
   );
-};
+}
 
 export default Timeline;
